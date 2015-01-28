7708da9e1cbe69073b579a61d3444b34c7551469
fans656 <fans656@yahoo.com>
2015-01-29 00:01:56 +0800

    Show English in widget using QTextLayout
    backspace and return worked too.

--- t.py
+++ t.py
@@ -7,60 +7,70 @@ from PySide.QtGui import *
 
 import util
 
-class Text(object):
+def show(text):
+    n = 79
+    print '*' * n
+    for line in text.split('\n'):
+        print '*' + line + ' ' * max(0, n - len(line) - 2) + '*'
+    print '*' * n
+    print
 
-    def __init__(self):
-        self.s = u''
-
-    def backspace(self):
-        self.s = self.s[:-1]
-        self.show()
-
-    def __iadd__(self, other):
-        self.s += other
-        self.show()
-        return self
-
-    def show(self):
-        print repr(self.s)
-
-class Edi(QDialog):
+class Widget(QDialog):
 
     __metaclass__ = util.metaclass
 
     def __init__(self, parent=None):
         self.super().__init__(parent)
-        self.text = Text()
-        self.setAttribute(Qt.WA_InputMethodEnabled)
-        self.setFocusPolicy(Qt.StrongFocus)
-        self.setFocus()
-        self.resize(320, 240)
+        self.text = u''
+        self.layouts = []
 
-    def keyPressEvent(self, event):
-        ch = event.text()
-        if ch == '\b':
-            self.text.backspace()
-        elif ch and ch in '\r\n':
+    def keyPressEvent(self, ev):
+        ch = ev.text()
+        key = ev.key()
+        # \r 属于 string.printable ，所以要优先判断
+        if ch and ch in '\r\n':
             self.text += '\n'
-        elif ch in string.printable:
+        elif key == Qt.Key_Backspace:
+            self.text = self.text[:-1]
+        elif ch and ch in string.printable:
             self.text += ch
+        self.layout()
+
+    def layout(self):
+        lines = self.text.split('\n')
+        self.layouts = []
+        for line in lines:
+            self.layoutLine(line)
         self.update()
-        self.super().keyPressEvent(event)
 
-    def inputMethodEvent(self, event):
-        s = event.commitString()
-        if s:
-            self.text += s
-            self.update()
+    def layoutLine(self, line):
+        fm = self.fontMetrics()
+        self.fm = fm
+        leading = fm.leading()
+        y = 0.0
+        lt = QTextLayout(line)
+        lt.setCacheEnabled(True)
+        lt.beginLayout()
+        while True:
+            line = lt.createLine()
+            if not line.isValid():
+                break
+            line.setLineWidth(self.width())
+            y += leading
+            line.setPosition(QPointF(0.0, y))
+            y += line.height()
+        lt.endLayout()
+        self.layouts.append(lt)
 
-    def paintEvent(self, event):
-        p = QPainter(self)
-        font = p.font()
-        font.setPointSize(40)
-        p.setFont(font)
-        p.drawText(self.rect(), self.text.s)
+    def paintEvent(self, ev):
+        painter = QPainter(self)
+        pt = QPoint(0, 0)
+        for lt in self.layouts:
+            lt.draw(painter, pt)
+            pt.setY(pt.y() + self.fm.leading() + self.fm.height())
+        show(self.text)
 
 app = QApplication(sys.argv)
-edi = Edi()
-edi.show()
+w = Widget()
+w.show()
 app.exec_()
